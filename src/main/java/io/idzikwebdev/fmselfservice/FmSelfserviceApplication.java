da636a4aadc241c71237ea6479fdc5c93ea9065f
Tomasz Idzik <idzikwebdev@gmail.com>
2023-07-25 12:29:07 +0200

    init

---
+++ src/main/java/io/idzikwebdev/fmselfservice/FmSelfserviceApplication.java
@@ -0,0 +1,13 @@
+package io.idzikwebdev.fmselfservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmSelfserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmSelfserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/idzikwebdev/fmselfservice/FmSelfserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.idzikwebdev.fmselfservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmSelfserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
